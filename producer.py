7a927cc8c520af965906e18149a21a0790e0a6f4
Andrew Scott <andrewscott@andrewsc-ltm.internal.salesforce.com>
2018-10-24 10:18:56 +0100

    updated prducer

--- producer.py
+++ producer.py
@@ -6,13 +6,13 @@ topic = "{}temp".format(os.environ["KAFKA_PREFIX"])
 
 print ("START! {}".format(topic))
 
-# producer = kafka_helper.get_kafka_producer()
+producer = kafka_helper.get_kafka_producer()
 
-# index = 0
-# while index < 200000:
-#     producer.send(topic, value={"temp":index})
-#     print ("SENT {}!".format(index))
-#     index+=1
+index = 0
+while index < 200000:
+    producer.send(topic, value={"temp":index})
+    print ("SENT {}!".format(index))
+    index+=1
 
 consumer = kafka_helper.get_kafka_consumer(topic=topic)
 print ("Connected")
